cb47c1133f2f8fed921bdf1f340f6579a2106bad
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-22 20:55:03 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,53 +1,45 @@
-bundle install
-def my_collection
-  describe "my_collect" do
-  let(:languages) { ['ruby', 'javascript', 'python', 'objective-c'] }
-  let(:students) { ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] }
-
-  it "can handle an empty collection" do
-    empty_array = []
-    counter = 0
-      my_collect(empty_array) do |x|
-        counter += 1
-      end
-    expect(counter).to eq(0)
-  end
-
-  it "yields the correct element from a given collection, in this case languages" do
-    my_collect(languages) do |language|
-      expect(language).to_not eq(nil)
-    end
-  end
-
-  it "returns a new collection of appropriately modified elements, in this case capitalized languages" do
-    expect(my_collect(languages) do |language|
-      language.upcase
-    end).to eq(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"])
-  end
-
-  it 'does not modify the original collection' do
-    my_collect(languages) do |language|
-      language.upcase
-    end
-    expect(languages).to eq(['ruby', 'javascript', 'python', 'objective-c'])
-  end
-
-  it "yields the correct element from the given collection, in this case students" do
-    my_collect(students) do |student|
-      expect(student).to_not eq(nil)
-    end
-  end
-
-  it "returns a new collection of appropriately modified elements, in this case student first names" do
-    expect(my_collect(students) do |student|
-      student.split(" ").first
-    end).to eq(["Tim", "Tom", "Sophie", "Antoin"])
-  end
-
-  it 'does not modify the original collection' do
-    my_collect(students) do |student|
-      student.split(" ").first
-    end
-    expect(students).to eq(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'])
-  end
+require "pry"
+def my_collect(array)
+     empty_array = []
+    counter = 0 
+    if counter <  empty_array.length 
+        my_collection (empty_array) do |say_it|
+        counter += 1 
+     end 
+   else 
+    puts "the block was empty"
 end
+emd 
+
+
+
+
+ my_collect (languages) do |language|
+  if block_given?
+      while counter < language.length do 
+        yield languages [i]
+      counter +=1 
+end 
+end 
+end 
+
+
+
+my_collect(languages) do |language|
+      languages = language.upcase 
+    end 
+  
+
+      
+   
+    my_collect (students) do |student|
+      counter == 1 
+      while counter < students.length 
+      student.split(" ").first
+      students << student 
+    end
+  end 
+end 
+
+
+
